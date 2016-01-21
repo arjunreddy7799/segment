e717b3197e3b548fb9e76c9bc47193970679ad60
saikrishna <saikrishna@saikrishnas-macbook-pro.local>
2016-01-21 11:26:15 +0530

    Initial Commit

---
+++ segment/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  segment
+//
+//  Created by saikrishna on 21/01/16.
+//  Copyright © 2016 saikrishna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ segmentTests/segmentTests.m
@@ -0,0 +1,39 @@
+//
+//  segmentTests.m
+//  segmentTests
+//
+//  Created by saikrishna on 21/01/16.
+//  Copyright © 2016 saikrishna. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface segmentTests : XCTestCase
+
+@end
+
+@implementation segmentTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
