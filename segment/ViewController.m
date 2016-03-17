13127fec97a5b2c100d04b0f45416e5759dff64b
saikrishna <saikrishna@saikrishnas-mbp.connectify>
2016-03-17 10:46:30 +0530

    segment code

--- segment/ViewController.m
+++ segment/ViewController.m
@@ -17,6 +17,26 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    NSArray *values=[[NSArray alloc]initWithObjects:@"daily",@"weakly",@"yearly", nil];
+    UISegmentedControl *segmentcontroller=[[UISegmentedControl alloc]initWithItems:values];
+    segmentcontroller.frame=CGRectMake(20, 20, 200, 50);
+    [self.view addSubview:segmentcontroller];
+    [segmentcontroller addTarget:self action:@selector(segmentvaluechanged:) forControlEvents:UIControlEventValueChanged ];
+    
+    UIScrollView *scroll=[[UIScrollView alloc]initWithFrame:CGRectMake(0, 20, 200, 250)];
+    scroll.contentSize=CGSizeMake(500, 900);
+    [self.view addSubview:scroll];
+    [scroll addSubview:segmentcontroller];
+
+    
+}
+-(void)segmentvaluechanged:(UISegmentedControl *)segment{
+    NSInteger *segmentvalue=segment.selectedSegmentIndex;
+    if(segmentvalue==0){
+        NSLog(@"1");
+    }
+    
 }
 
 - (void)didReceiveMemoryWarning {
